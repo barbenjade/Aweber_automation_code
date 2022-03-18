038bcdf4ab2d0d2d9e7d4adca552cd9c51f73959
Jade Barben <41709617+barbenjade@users.noreply.github.com>
2022-03-17 18:24:04 -0600

    Delete one.py

--- one.py
+++
@@ -1,8 +0,0 @@
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium import webdriver
-#
-#
-# s = Service(ChromeDriverManager().install())
-# driver = webdriver.Chrome(service=s)
-#
-# driver.get("https://wordpress.com/me")
